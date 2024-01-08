72ac29efdda482789290cb11eabf499587bccc7e
MohamedRagabAbbas <Mohamed_Ragab02@aucegypt.edu>
2024-01-08 23:12:50 +0200

    add login form and validators

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import {routes} from '../app.routes';
-import {FormBuilder, FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {FloatLabelType, MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
 import {MatRadioModule} from '@angular/material/radio';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
@@ -20,19 +21,56 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
-    MatIconModule,],
+    MatIconModule,
+    CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  formGroup: FormGroup;
 
-  
-  options = this._formBuilder.group({
+  constructor(private _formBuilder: FormBuilder) {
+    this.formGroup = this._formBuilder.group({
+      email: ['', [Validators.required, Validators.email,Validators.pattern('^[a-zA-Z0-9_.]+@[a-zA-Z0-9-]+[\.][a-zA-Z]{2,}$')]],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.pattern('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$')]],
+      isStdeunt: ['', [Validators.required]],
+    });
+  }
+
+  get email() {
+    return this.formGroup.get('email') as FormControl;
+  }
+
+  get password() {
+    return this.formGroup.get('password') as FormControl;
+  }
+
+  get isStudent() {
+    return this.formGroup.get('isStudent') as FormControl;
+  }
+
+  getErrorMessage(control: FormControl) {
+    if (control.hasError('required')) {
+      return 'You must enter a value';
+    }
+    if(control===this.email)
+    {
+      if (this.email.hasError('pattern')) {
+        return 'Not a valid email';
+      }
+    }
     
-  });
-  
-
-
-  constructor(private _formBuilder: FormBuilder) {}
+    if(control===this.password)
+    {
+      if (this.password.hasError('required')) {
+        return 'You must enter a value';
+      }
+      if (this.password.hasError('pattern')) {
+        return 'Not a valid password';
+      }
+    }
+    
+    return '';
+  }
   
 }
